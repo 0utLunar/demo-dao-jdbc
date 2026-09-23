9ee2805e5cbbea43cd734c431b04153d2faa7692
0utLunar <contatolucascir@gmail.com>
2026-09-23 17:13:21 -0300

    findAll implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -28,6 +28,13 @@ public class Program {
             System.out.println();
         }
 
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.printf(obj.toString());
+            System.out.println();
+        }
+
 
 
     }
--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -117,6 +117,47 @@ public class SellerDaoJDBC implements SellerDao {
         }
     }
 
+    @Override
+    public List<Seller> findAll() {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            st = conn.prepareStatement(
+                    "SELECT seller.*, department.Name as DepName FROM seller "+
+                            "INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "ORDER BY Name"
+            );
+
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
     private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
         return new Seller(
                 rs.getInt("Id"),
@@ -135,10 +176,7 @@ public class SellerDaoJDBC implements SellerDao {
         );
     }
 
-    @Override
-    public List<Seller> findAll() {
-        return List.of();
-    }
+
 
 
 }
